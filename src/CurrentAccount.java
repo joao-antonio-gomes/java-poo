8152c5c0672cb518a0fa151d9697c16a81d25feb
João Antônio Gomes <66046251+joao-antonio-gomes@users.noreply.github.com>
2021-09-18 10:33:34 -0300

    Add Current and Deposit accounts who are sub-class from Account

---
+++ src/CurrentAccount.java
@@ -0,0 +1,5 @@
+public class CurrentAccount extends Account {
+    public CurrentAccount(int agency, int number) {
+        super(agency, number);
+    }
+}
---
+++ src/DepositAccount.java
@@ -0,0 +1,5 @@
+public class DepositAccount extends Account {
+    public DepositAccount(int agency, int number) {
+        super(agency, number);
+    }
+}
