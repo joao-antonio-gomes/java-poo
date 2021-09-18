696c6270d36fae1a8b24e523c50cbc9957dd8276
João Antônio Gomes <66046251+joao-antonio-gomes@users.noreply.github.com>
2021-09-17 22:42:28 -0300

    Começando a trabalhar com heranças

--- src/Conta.java
+++ src/Conta.java
@@ -1,6 +1,68 @@
 public class Conta {
-    double saldo;
-    int agencia;
-    int numero;
-    String titular;
+    private double saldo;
+    private int agencia = 42;
+    private int numero;
+    private Pessoa titular;
+    private static int total;
+
+    public Conta(int agencia, int numero) {
+        Conta.total++;
+        this.agencia = agencia;
+        this.numero = numero;
+    }
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    public int getNumero() {
+        return this.numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getAgencia() {
+        return this.agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public Pessoa getTitular() {
+        return this.titular;
+    }
+
+    public void setTitular(Pessoa titular) {
+        this.titular = titular;
+    }
+
+    public static int getTotalDeContas() {
+        return total;
+    }
+
+    public void deposita(double valor) {
+        this.saldo += valor;
+        System.out.println("O saldo da conta do " + this.getTitular().getNome() +
+                " é de R$ " + this.getSaldo());
+    }
+
+    public boolean saca(double valor) {
+        if (valor > 0 && this.saldo >= valor) {
+            valor = this.saldo - valor;
+            this.saldo = valor;
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean transfere(Conta contaDestino, double valor) {
+        if (this.saca(valor)) {
+            contaDestino.deposita(valor);
+            return true;
+        }
+        return false;
+    }
 }
--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,2 +1,29 @@
-package PACKAGE_NAME;public class Pessoa {
+public class Pessoa {
+    private String nome;
+    private String cpf;
+    private String profissao;
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getProfissao() {
+        return this.profissao;
+    }
+
+    public void setProfissao(String profissao) {
+        this.profissao = profissao;
+    }
 }
