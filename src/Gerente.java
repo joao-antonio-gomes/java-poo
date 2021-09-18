a8c0d5cc846fbfa206784085039f353e16889b5f
João Antônio Gomes <66046251+joao-antonio-gomes@users.noreply.github.com>
2021-09-18 08:53:03 -0300

    Excluido classes anteriores para refatoração: criado Funcionario (super class) e Gerente (extend Funcionario

--- src/Gerente.java
+++ src/Gerente.java
@@ -1,33 +1,11 @@
-public class Funcionario {
-    private String nome;
-    private String cpf;
-    private double salario;
-    
+public class Gerente extends Funcionario {
+    private int senha;
+
+    public boolean autentica(int senha) {
+        return this.senha == senha;
+    }
+
     public double getBonificacao() {
-        return this.salario * 0.1;
-    }
-    
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public double getSalario() {
-        return salario;
-    }
-
-    public void setSalario(double salario) {
-        this.salario = salario;
+        return super.salario * 1;
     }
 }
--- src/TesteFuncionario.java
+++ src/TesteFuncionario.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class TesteFuncionario {
+public class TesteFuncionario {
+    public static void main(String[] args) {
+        Gerente g1 = new Gerente();
+        g1.setNome("João");
+        g1.setSalario(1000);
+
+        System.out.println(g1.getNome());
+        System.out.println(g1.getBonificacao());
+    }
 }
